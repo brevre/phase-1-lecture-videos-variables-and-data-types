51e21744e4ec917bad35ebf168a7d2d34517ca46
brevre <brendamakena09@gmail.com>
2023-09-27 22:40:57 +0200

    completed

---
+++ intro.js
@@ -0,0 +1,40 @@
+age = 1
+console.log(typeof age)
+console.log(age)
+
+myVar = 'cat'
+console.log(typeof myVar)
+console.log(myVar)
+
+myvar2 = "cat"
+console.log(typeof myvar2)
+console.log(myvar2)
+
+myVar3 = "1"
+console.log(typeof myVar3)
+console.log(myVar3)
+
+myvar4 = true 
+console.log(typeof myvar4)
+console.log(myvar4)
+
+myvar5 = false
+console.log(typeof myvar5)
+console.log(myvar5)
+
+myvar6 = undefined
+console.log(typeof myvar6)
+console.log(myvar6)
+
+myvar7 = null
+console.log(typeof myvar7)
+console.log(myvar7)
+
+
+
+
+
+
+
+
+
